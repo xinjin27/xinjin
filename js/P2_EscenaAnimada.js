9b4ea85e05f58b560141b461002d8e2840197dc0
xinjin27 <xinjj27@gmail.com>
2024-02-26 22:43:20 +0100

    prueba

---
+++ js/P2_EscenaAnimada.js
@@ -0,0 +1,144 @@
+/**
+ * EscenaAnimada.js
+ * 
+ * Practica AGM #2. Escena basica con interfaz y animacion
+ * Se trata de añadir un interfaz de usuario que permita 
+ * disparar animaciones sobre los objetos de la escena con Tween
+ * 
+ * @author 
+ * 
+ */
+
+// Modulos necesarios
+/*******************
+ * TO DO: Cargar los modulos necesarios
+ *******************/
+import * as THREE from "../lib/three.module.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN} from "../lib/tween.module.min.js";
+import {GUI} from "../lib/lil-gui.module.min.js";
+// Variables de consenso
+let renderer, scene, camera;
+
+// Otras globales
+/*******************
+ * TO DO: Variables globales de la aplicacion
+ *******************/
+let angulo = 0;
+
+// Acciones
+init();
+loadScene();
+loadGUI();
+render();
+
+function init()
+{
+    // Motor de render
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    /*******************
+    * TO DO: Completar el motor de render y el canvas
+    *******************/
+    document.getElementById('container').appendChild( renderer.domElement );
+
+    // Escena
+    scene = new THREE.Scene();
+    
+    // Camara
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
+    camera.position.set( 0.5, 2, 7 );
+    /*******************
+    * TO DO: Añadir manejador de camara (OrbitControls)
+    *******************/
+    cameraControls = new OrbitControls( camera, renderer.domElement ); 
+    cameraControls.target.set(0,1,0);
+    camera.lookAt( new THREE.Vector3(0,1,0) );
+}
+
+function loadScene()
+{
+    const material = new THREE.MeshNormalMaterial( {wireframe:false} );
+
+    /*******************
+    * TO DO: Misma escena que en la practica anterior
+    *******************/
+
+    const geoCubo = new THREE.BoxGeometry( 2,2,2 );
+    const geoEsfera = new THREE.SphereGeometry( 1, 20,20 );
+
+    const cubo = new THREE.Mesh( geoCubo, material );
+    const esfera = new THREE.Mesh( geoEsfera, material );
+
+    // Suelo
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
+    suelo.rotation.x = -Math.PI / 2;
+    scene.add(suelo);
+
+    // Importar un modelo en json
+    const loader = new THREE.ObjectLoader();
+
+    loader.load( 'models/soldado/soldado.json', 
+        function(objeto){
+            cubo.add(objeto);
+            objeto.position.y = 1;
+        }
+    )
+
+    // Importar un modelo en gltf
+    const glloader = new GLTFLoader();
+
+    glloader.load( 'models/RobotExpressive.glb', function ( gltf ) {
+    //glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
+        gltf.scene.position.y = 1;
+        gltf.scene.rotation.y = -Math.PI/2;
+        esfera.add( gltf.scene );
+        console.log("ROBOT");
+        console.log(gltf);
+    
+    }, undefined, function ( error ) {
+    
+        console.error( error );
+    
+    } );
+
+    esferaCubo = new THREE.Object3D();
+    esferaCubo.position.y = 1.5;
+    cubo.position.x = -1;
+    esfera.position.x = 1;
+    cubo.add( new THREE.AxesHelper(1) );
+
+    scene.add( esferaCubo);
+    esferaCubo.add( cubo );
+    esferaCubo.add( esfera );
+ 
+
+    scene.add( new THREE.AxesHelper(3) );
+}
+
+function loadGUI()
+{
+    // Interfaz de usuario
+    /*******************
+    * TO DO: Crear la interfaz de usuario con la libreria lil-gui.js
+    * - Funcion de disparo de animaciones. Las animaciones deben ir
+    *   encadenadas
+    * - Slider de control de radio del pentagono
+    * - Checkbox para alambrico/solido
+    *******************/
+}
+
+function update(delta)
+{
+    /*******************
+    * TO DO: Actualizar tween
+    *******************/
+}
+
+function render(delta)
+{
+    requestAnimationFrame( render );
+    update(delta);
+    renderer.render( scene, camera );
+}
